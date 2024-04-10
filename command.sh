cdc8899e9ff6a8e2483ad60e66b61c0d50baf51d
Suraj Mahto <surajmahto255@gmail.com>
2024-04-10 11:27:48 +0530

    first commit

---
+++ command.sh
@@ -0,0 +1,24 @@
+C:\Users\suraj\Desktop\CandyMachine
+
+# CN5PuFdvr9RmDJYKWfcHYXgAhXYY3Dkbidp7rfkLdoQu
+solana-keygen new --outfile C:\Users\suraj\Desktop\CandyMachine\Owner.json
+
+# 3MDDLkJoik7Pf7mgRwNvyMP7sBNazxuzg2KxH5WtUrKe
+solana-keygen new --outfile C:\Users\suraj\Desktop\CandyMachine\Creator.json
+
+solana config set --keypair C:\Users\suraj\Desktop\CandyMachine\Owner.json
+
+solana config set --url https://api.devnet.solana.com
+
+solana airdrop 1 CN5PuFdvr9RmDJYKWfcHYXgAhXYY3Dkbidp7rfkLdoQu --url https://api.devnet.solana.com
+solana airdrop 1 3MDDLkJoik7Pf7mgRwNvyMP7sBNazxuzg2KxH5WtUrKe --url https://api.devnet.solana.com
+solana airdrop 1 7CqJ1J9FSA1BgQGgoXSofDrQg1RF246evAb5b8hCUBze --url https://api.devnet.solana.com
+
+Collection mint ID: GSQtRQ34aCjhw5QZbBrdocVqq6LGX6JwjBuvdGrR6jni
+
+Candy machine ID: HJWabBSuWoc7S4NA3mJ6xE39jbzHxQ3FE61BVEpn78gC
+
+See your candy machine at:
+  -> https://www.solaneyes.com/address/HJWabBSuWoc7S4NA3mJ6xE39jbzHxQ3FE61BVEpn78gC?cluster=devnet
+
+
